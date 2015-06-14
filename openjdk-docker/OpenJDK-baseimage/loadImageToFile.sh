19fd6e2a903269a9af9a01ac46ebe987493e4dfc
neomatrix369 <sadhak001@gmail.com>
2015-06-14 23:28:24 +0100

    Renamed loadImage...sh to loadImageFromTarGZip, also corrected redirecting of TarGZip into 'docker load' command

--- openjdk-docker/OpenJDK-baseimage/loadImageToFile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-IMAGE_ARCHIVE_FILE="openjdk9-base-image.tar.gz"
-
-echo "Loading docker image from $IMAGE_ARCHIVE_FILE."
-docker load < $IMAGE_ARCHIVE_FILE
-echo "Finished loading docker image into local repository."
--- openjdk-docker/OpenJDK9/loadImageToFile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-IMAGE_ARCHIVE_FILE="openjdk9-full-image.tar.gz"
-
-echo "Loading docker image from $IMAGE_ARCHIVE_FILE."
-docker load < gzip < $IMAGE_ARCHIVE_FILE
-echo "Finished loading docker image into local repository."
