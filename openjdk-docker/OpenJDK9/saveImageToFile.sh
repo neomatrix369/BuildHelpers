c9941d97cf8530b044b4b54ea4ef1aefd551de2a
neomatrix369 <sadhak001@gmail.com>
2015-06-14 19:28:01 +0100

    Added scripts to load archives into local repo, amended save image to file.

--- openjdk-docker/OpenJDK9/saveImageToFile.sh
+++ openjdk-docker/OpenJDK9/saveImageToFile.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-IMAGE_ARCHIVE_FILE="openjdk9-full-image.tar"
+IMAGE_ARCHIVE_FILE="openjdk9-full-image.tar.gz"
 DOCKER_IMAGE="neomatrix369/openjdk9-full-image:latest"
 
 echo "Creating $IMAGE_ARCHIVE_FILE from Docker image $DOCKER_IMAGE"
