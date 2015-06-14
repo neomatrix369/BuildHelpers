8c06ce1b8451ffe69764949666199e601e6244c4
neomatrix369 <sadhak001@gmail.com>
2015-06-14 19:38:41 +0100

    Removed th Docker folder and move Dockerfiles into respective folder

--- Docker/OpenJDK-baseimage/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-echo "Building the Dockerfile with tag 'base-image/openjdk9'"
-docker build -t base-image/openjdk9 .
--- Docker/OpenJDK9/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-echo "Building the Dockerfile with tag 'full-image/openjdk9'"
-docker build -t full-image/openjdk9 .
